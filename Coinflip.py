d36c45e6c24cb6f20f0a15a6b2b54e752fe56dbb
johnnylapelle <jvacossinj@gmail.com>
2025-01-13 09:46:04 +0100

    Add files via upload

--- Coinflip.py
+++ Coinflip.py
@@ -1,33 +1,39 @@
-# Jeu pile ou face
-# To do :
-# - GUI + Animation de coinflip
 import random
 
-list = ['pile', 'face']
+list_pileface = ['pile', 'face']
+list_ouinon = ['oui', 'non']
 points = 0
 winstreak = 0
+
 
 while True:
     print("Voulez-vous jouer ?")
     reponse = input("oui ou non ?")
-
-    if reponse == "non":
+    if reponse not in list_ouinon:
+        print("OUI ou NON")
+    elif reponse == "non":
         break
-    
-    flip = input("Pile ou Face ?")
-    generated_word = (random.choice(list))
-    print(f"La pièce est tombée sur : {generated_word}")
+    else :
+        flip = input("Pile ou Face ?").strip().lower()
 
-    if generated_word == flip:
-        print("Bravo ! +1 Points !")
-        points += 1
-        winstreak += 1
+        if flip in list_pileface:
+            
+            generated_word = (random.choice(list_pileface))
+            print(f"La pièce est tombée sur : {generated_word}")
 
-    if generated_word != flip:
-        print("Dommage :(")     
-        winstreak = 0
+            if generated_word == flip:
+                print("Bravo ! +1 Points !")
+                points += 1
+                winstreak += 1
 
-    print(f"Votre score est de : {points}")
-    print(f"Votre Winstreak est de : {winstreak}")
+            if generated_word != flip:
+                print("Dommage :(")     
+                winstreak = 0
+
+            print(f"Votre score est de : {points}")
+            print(f"Votre Winstreak est de : {winstreak}")
+
+        else :
+            print("NON")
 
 print("Bye!")
